fc4c90957e27e461e5c34e068376da952e002d22
Katharine Berry <katharine@kathar.in>
2018-02-07 21:36:53 -0800

    Actually use config.

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,9 +1,5 @@
-import logging
-logging.basicConfig(level=logging.DEBUG)
 from flask import Flask
 from flask_login import login_required
-
-# logging.getLogger('flask_oauthlib').setLevel(logging.DEBUG)
 
 from .models import init_app as init_db
 from .login import init_app as init_login
@@ -11,7 +7,11 @@ from .login.pebble import ensure_pebble
 from .oauth import init_app as init_oauth
 from .api import init_app as init_api
 
+from .settings import config
+
+
 app = Flask(__name__)
+app.config.update(**config)
 init_db(app)
 init_login(app)
 init_oauth(app)
