fe2d1c884f46fa2c289d4b9aaf5f47de11a18a93
Joshua Wise <joshua@joshuawise.com>
2018-03-17 22:14:13 -0700

    We would generate a SERVER_NAME from DOMAIN_ROOT, except that that currently is pending on pallets/flask#2635.  So comment it out for now.

--- auth/settings.py
+++ auth/settings.py
@@ -2,7 +2,7 @@ from os import environ
 
 config = {
     'SECRET_KEY': environ['SECRET_KEY'],
-    'SERVER_NAME': environ.get('SERVER_NAME'),
+#    'SERVER_NAME': f"auth.{environ['DOMAIN_ROOT']}",
     'SQLALCHEMY_DATABASE_URI': environ['DATABASE_URL'],
     'AUTH_GOOGLE': {
         'consumer_key': environ['GOOGLE_CONSUMER_KEY'],
