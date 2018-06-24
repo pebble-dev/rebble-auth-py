9ce8fb5366463509b474004c92beb4c21d0381a5
Katharine Berry <katharine@kathar.in>
2018-06-24 15:42:31 -0700

    Fix some bugs.

--- auth/api.py
+++ auth/api.py
@@ -8,7 +8,7 @@ api = Blueprint('api', __name__)
 @api.route('/me')
 @oauth.require_oauth('profile')
 def me():
-    return jsonify(uid=request.oauth.user.id)
+    return jsonify(uid=request.oauth.user.id, name=request.oauth.user.name)
 
 
 @api.route('/me/pebble/auth')
@@ -19,16 +19,16 @@ def pebble_auth_me():
     return jsonify(id=user.pebble_auth_uid, email=user.email, name=user.name)
 
 
-@api.route('/me/pebble/dev-portal')
+@api.route('/me/pebble/appstore')
 @oauth.require_oauth('pebble')
 @api_ensure_pebble
 def pebble_dev_portal_me():
     user = request.oauth.user
-    return jsonify(users=[{
+    return jsonify({
         'id': user.pebble_dev_portal_uid,
         'uid': user.pebble_auth_uid,
         'rebble_id': user.id,
-    }])
+    })
 
 
 def init_app(app, url_prefix='/api/v1'):
