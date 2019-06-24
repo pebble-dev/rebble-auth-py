pebble-dev/rebble-auth-py

commit 559f4cd38171a446160044006d77b00624a9d60d

`deleted` is a property only of a Stripe customer that has been baleeted.

auth/billing/__init__.py
@@ -47,7 +47,7 @@ def create_subscription():
     customer = None
     if current_user.stripe_customer_id:
         customer = stripe.Customer.retrieve(current_user.stripe_customer_id)
-        if not customer.deleted:
+        if (deleted not in customer) or (not customer.deleted):
             customer.source = request.form['stripeToken']
             try:
                 customer.save()

commit 36e478802c38d05bd64bef79253ae39c97760c5d

Real dumb fix for real dumb issue introduced by 559f4cd, tested equally well.

auth/billing/__init__.py
@@ -47,7 +47,7 @@ def create_subscription():
     customer = None
     if current_user.stripe_customer_id:
         customer = stripe.Customer.retrieve(current_user.stripe_customer_id)
-        if (deleted not in customer) or (not customer.deleted):
+        if ('deleted' not in customer) or (not customer.deleted):
             customer.source = request.form['stripeToken']
             try:
                 customer.save()